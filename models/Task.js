a6990df3cecbf4bf342ac64ee9b3bcc8451f42dc
Yan-Carvalho <yan.carvalho2@hotmail.com>
2022-11-02 18:45:00 -0300

    finish task and schema

---
+++ models/Task.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//Definindo o schema necessário para o mongoose
+//Defining the schema needed for mongoose
+
+const taskSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    require: true,
+  },
+  check: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
