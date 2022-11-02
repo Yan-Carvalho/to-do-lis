197eb07dd4f505f622e01ea78071609a81aafe04
Yan-Carvalho <yan.carvalho2@hotmail.com>
2022-11-02 19:16:02 -0300

    Routes configurations is finished

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,10 @@ const routes = require("express").Router();
 const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
+routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
+routes.get("/check/:id", TaskController.taskCheck);
 
 module.exports = routes;
