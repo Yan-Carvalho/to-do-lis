afcc9e83939891830d1f33c809da5928ea53e4a0
Yan-Carvalho <yan.carvalho2@hotmail.com>
2022-11-04 13:49:31 -0300

    auto refresh

--- public/script.js
+++ public/script.js
@@ -8,3 +8,17 @@ closeMessage.addEventListener("click", () => {
 setTimeout(() => {
   message.style.display = "none";
 }, 5000);
+
+$(document).ready(function(){
+  var counter = 9;
+window.setInterval(function(){
+counter = counter - 3;
+if(counter>=0){
+  document.getElementById('off').innerHTML=counter;
+}
+if (counter===0){
+  counter=9;
+}
+$("#tasks").load(window.location.href + " #tasks" );
+}, 10);
+});
